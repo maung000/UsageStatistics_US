5c223e934bb6dca5894ccf8090802ef04abb92dd
nghiant <nghiant@rikkeisoft.com>
2019-12-10 14:53:46 +0700

    Update set lock app

--- src/main/java/ca/mimic/usagestatistics/Activity/Settings.java
+++ src/main/java/ca/mimic/usagestatistics/Activity/Settings.java
@@ -245,10 +245,12 @@ public class Settings extends Activity implements ActionBar.TabListener {
 
 
         sharedPreference = new SharedPreference();
-        String password = sharedPreference.getPassword(mContext);
-//        boolean checkSetPinCode = sharedPreference.getCheckSetPinCode(mContext);
-//        if (!checkSetPinCode)
-        launchCreatePassword(mContext);
+        String password = sharedPreference.getPasswordApp(mContext);
+        if (TextUtils.isEmpty(password)) {
+            launchCreatePassword(mContext);
+        } else {
+
+        }
 
         if (mIsAtLeastLollipop && needsUsPermission()) {
             launchUsPermission(mContext);
@@ -457,18 +459,7 @@ public class Settings extends Activity implements ActionBar.TabListener {
                                 startActivity(new Intent(mContext, PasswordSelectLock.class));
                             }
                         })
-                .setNegativeButton(R.string.btn_ignore, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .setOnCancelListener(new DialogInterface.OnCancelListener() {
-                    @Override
-                    public void onCancel(DialogInterface dialog) {
-                        dialog.dismiss();
-                    }
-                })
+                .setCancelable(false)
                 .show();
 
     }
